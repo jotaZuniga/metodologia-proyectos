0b2d099e80fec138a2b6a01fecc511927fc066af
Jose Zuniga <jzuniga@appvance.com>
2023-02-12 22:09:12 -0600

    #2

---
+++ classA.js
@@ -0,0 +1,20 @@
+class ClaseA {
+  foo;
+  bar;
+
+  setFoo(val) {
+    this.foo = val;
+  }
+
+  setBar(val) {
+    this.bar = val;
+  }
+
+  get foo() {
+    return this.foo;
+  }
+
+  get bar() {
+    return this.bar
+  }
+}
